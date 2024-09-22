ea8724a6a0963da3622e198be12c3f98a9953010
marvel084 <moonhikari084@gmail.com>
2024-09-23 04:44:28 +0800

    Add testfont

---
+++ testfonts/makeclearall.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+## Copyright (c) 2012-2016 hzz
+
+#clear aux files
+
+rm -rf *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb  
